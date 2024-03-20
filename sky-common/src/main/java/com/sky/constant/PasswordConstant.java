17b93f6b88a0228206c3f63a60305220dcf7706d
Hello-wxy <2239848423@qq.com>
2024-03-20 15:46:02 +0800

    新增员工代码开发

--- sky-common/src/main/java/com/sky/constant/PasswordConstant.java
+++ sky-common/src/main/java/com/sky/constant/PasswordConstant.java
@@ -1,5 +1,9 @@
 package com.sky.constant;
 
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * 密码常量
  */
@@ -7,4 +11,19 @@ public class PasswordConstant {
 
     public static final String DEFAULT_PASSWORD = "123456";
 
+    /**
+     * 计算默认密码的加密后密码的static方法
+     * @return
+     */
+    public static String DEFAULT_SECRETPASSWORD(){
+        MessageDigest messageDigest= null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(DEFAULT_PASSWORD.getBytes());
+            byte[] result=messageDigest.digest();
+            return new HexBinaryAdapter().marshal(result);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,15 +12,53 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeMapper employeeMapper;
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+
+        //System.out.println("当前线程id "+Thread.currentThread().getId());
+
+        Employee employee=new Employee();
+        //对象属性拷贝(因为dto和实体类的属性能够一一对应，此时就不用一个个set，而是使用对象的属性拷贝,前提是属性名必须一致）
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //由于实体类中还有状态、创建时间等更多的属性，因此需要继续设置
+
+        //设置账号的状态，默认正常状态 1表示正常 0表示锁定，由于直接set为1属于硬编码不便维护，因此使用常量StatusConstant
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //设置密码，新增员工默认密码123456，因为密码已经加密，因此需要使用SHA-256加密
+        employee.setPassword(PasswordConstant.DEFAULT_SECRETPASSWORD());
+
+        //设置创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //设置修改当前记录创建人的id和修改人id
+        //最初先写死，之后再通过新技术来改为当前登录用户的id
+        //通过ThreadLocal方法，利用common模块中的BaseContext类来获取CurrentId
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employeeMapper.insert(employee);
+
+    }
 
     /**
      * 员工登录
@@ -39,7 +80,18 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        //对前端传过来的明文密码进行SHA-256加密处理
+        try {
+            MessageDigest messageDigest=MessageDigest.getInstance("SHA-256");
+            messageDigest.update(password.getBytes());
+            byte[] result=messageDigest.digest();
+            password=new HexBinaryAdapter().marshal(result);
+            System.out.println("SHA-256 :"+password);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
